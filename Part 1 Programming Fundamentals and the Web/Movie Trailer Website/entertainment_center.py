82397224853b8c577707e8740a5b4a07c901a40c
suuoh <me@melvin.io>
2017-09-30 20:35:56 -0400

    Format to meet PEP8 guidelines

--- Part 1 Programming Fundamentals and the Web/Movie Trailer Website/entertainment_center.py
+++ Part 1 Programming Fundamentals and the Web/Movie Trailer Website/entertainment_center.py
@@ -1,51 +1,67 @@
 import media
 import fresh_tomatoes
 
+# Create objects for a set of movies with
+# movie title, storyline, poster URL, and trailer URL
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://upload.wikimedia.org/"
+                        "wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 finding_nemo = media.Movie("Finding Nemo",
-                           "A fish gets separated from his parents in the ocean.",
-                           "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",
+                           "A fish gets separated from his parents.",
+                           "https://upload.wikimedia.org/"
+                           "wikipedia/en/2/29/Finding_Nemo.jpg",
                            "https://www.youtube.com/watch?v=wZdpNglLbt8")
 
 interstellar = media.Movie("Interstellar",
-                           "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-                           "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+                           "A team of explorers travel through space in "
+                           "an attempt to ensure humanity's survival.",
+                           "https://upload.wikimedia.org/wikipedia/"
+                           "en/b/bc/Interstellar_film_poster.jpg",
                            "https://www.youtube.com/watch?v=0vxOhd4qlnA")
 
 incredibles = media.Movie("The Incredibles",
-                          "A family of undercover superheroes are forced into action to save the world.",
-                          "https://upload.wikimedia.org/wikipedia/en/e/ec/The_Incredibles.jpg",
+                          "A family of superheroes save the world.",
+                          "https://upload.wikimedia.org/wikipedia/"
+                          "en/e/ec/The_Incredibles.jpg",
                           "https://www.youtube.com/watch?v=eZbzbC9285I")
 
 hunger_games = media.Movie("Hunger Games",
                            "Survival of the fittest.",
-                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                           "http://upload.wikimedia.org/wikipedia/"
+                           "en/4/42/HungerGamesPoster.jpg",
                            "https://www.youtube.com/watch?v=PbA63a7H0bo")
 
 now_you_see_me = media.Movie("Now You See Me",
-                             "A team of illusionists pull off bank heists during their performances.",
-                             "http://t3.gstatic.com/images?q=tbn:ANd9GcSiVU-YMu-9iM1YT68QCa322Q_fSe8D9DmmCRahPfTY9evxLty2",
+                             "A team of illusionists pull off bank "
+                             "heists during their performances.",
+                             "http://t3.gstatic.com/images?q=tbn:ANd9GcSiVU-YM"
+                             "u-9iM1YT68QCa322Q_fSe8D9DmmCRahPfTY9evxLty2",
                              "https://www.youtube.com/watch?v=KzJNYYkkhzc")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet.",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "http://upload.wikimedia.org/wikipedia/"
+                     "id/b/b0/Avatar-Teaser-Poster.jpg",
                      "http://www.youtube.com/watch?v=-9ceBgWV8io")
 
 avengers = media.Movie("The Avengers",
-                       "Earth's mightiest heroes must come together and learn to fight as a team.",
-                       "http://t1.gstatic.com/images?q=tbn:ANd9GcTp0qlAoWcOOswIkL_qpjYzJqCCDmWXiBzCXiqbE43Obo8c0Z-s",
+                       "Earth's mightiest heroes must come together "
+                       "and learn to fight as a team.",
+                       "http://t1.gstatic.com/images?q=tbn:ANd9GcTp0qlAoWcOOsw"
+                       "IkL_qpjYzJqCCDmWXiBzCXiqbE43Obo8c0Z-s",
                        "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
 ghost_shell = media.Movie("Ghost in the Shell",
-                          "A cyber-enhanced soldier must stop the world's most dangerous criminals.",
-                          "http://t3.gstatic.com/images?q=tbn:ANd9GcRtT-lG0Kw1lfxJZBJ21dCXZoHRqZ63drxjC-E3ji-PEh7d4PuE",
+                          "A cyber-enhanced soldier must stop the world's "
+                          "most dangerous criminals.",
+                          "http://t3.gstatic.com/images?q=tbn:ANd9GcRtT-lG0Kw"
+                          "1lfxJZBJ21dCXZoHRqZ63drxjC-E3ji-PEh7d4PuE",
                           "https://www.youtube.com/watch?v=G4VmJcZR0Yg")
 
+# Add all movies to array
 movies = [toy_story,
           finding_nemo,
           interstellar,
@@ -56,4 +72,5 @@ movies = [toy_story,
           avengers,
           ghost_shell]
 
+# Use fresh_tomatoes module that generates the HTML website
 fresh_tomatoes.open_movies_page(movies)
--- Part 1 Programming Fundamentals and the Web/Movie Trailer Website/media.py
+++ Part 1 Programming Fundamentals and the Web/Movie Trailer Website/media.py
@@ -2,10 +2,16 @@ import webbrowser
 
 
 class Movie(object):
-    """This class provides a way to store movie related information"""
+    """This class provides a way to store movie related information
 
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    Args:
+        movie_title: String of title of a movie
+        movie_storyline: String of storyline or plot
+        movie_poster:  String of URL to movie poster
+        movie_trailer: String of URL to movie trailer on YouTube
+    """
 
+    # Constructor
     def __init__(self, movie_title, movie_storyline,
                  movie_poster, movie_trailer):
         self.title = movie_title
@@ -13,5 +19,6 @@ class Movie(object):
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailer
 
+    # Open browser with movie trailer URL
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
