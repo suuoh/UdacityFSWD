83d108f4038bb4a57a1bbbea6e2fba392528478f
suuoh <me@melvin.io>
2018-01-10 23:43:51 -0500

    Simplify queries

--- Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/news/news.py
+++ Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/news/news.py
@@ -8,23 +8,21 @@ db = psycopg2.connect("dbname=news")
 c = db.cursor()
 
 # 1. Top 3 articles by views sorted in descending order
-c.execute("SELECT title, views FROM articles, (SELECT path, count(*) as views FROM articles, log WHERE path LIKE '%' || slug GROUP BY path ORDER BY views DESC) as articleviews WHERE path LIKE '%' || slug LIMIT 3;")
+c.execute("SELECT title, count(*) as pageviews FROM articles, log WHERE log.path LIKE '%' || articles.slug GROUP BY title ORDER BY pageviews DESC LIMIT 3;")
 for result in c:
     print result
 print
 
 # 2. Top authors by article views sorted in descending order
-c.execute("SELECT name, totalviews FROM authors LEFT JOIN (SELECT author, SUM(views) as totalviews FROM articles, (SELECT path, count(*) as views FROM articles, log WHERE path LIKE '%' || slug GROUP BY path) as articleviews WHERE path LIKE '%' || slug GROUP BY author) as authorviews ON authors.id = authorviews.author ORDER BY totalviews DESC;")
-for result in c:
-    print result
-print
 c.execute("SELECT authors.name, count(*) as totalviews FROM articles, log, authors WHERE log.path LIKE '%' || articles.slug and authors.id = articles.author GROUP BY name ORDER BY totalviews DESC;")
 for result in c:
     print result
 print
 
 # 3. Days with more than 1% request errors
-#c.execute()
-#print c.fetchall()
+c.execute("SELECT day, to_char(cast(access.error as decimal) / (access.error + access.ok) * 100, '999D99%') as errorrate FROM (SELECT date(log.time) as day, sum(CASE WHEN log.status LIKE '2%' THEN 1 ELSE 0 END) as ok, sum(CASE WHEN log.status LIKE '4%' THEN 1 ELSE 0 END) as error FROM log GROUP BY day ORDER BY day) as access;")
+for result in c:
+    print result
+print
 
 db.close()
