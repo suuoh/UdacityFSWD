e4f512aafc8c08938f2c05d04c5dab8f2762e12d
suuoh <me@melvin.io>
2018-01-11 23:03:36 -0500

    Finalize logs project with readme

---
+++ Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/news/README.md
@@ -0,0 +1,10 @@
+# Logs Analysis Project
+Reporting tool to find:
+1. The three most popular articles of all time
+2. The most popular authors of all time
+3. Days where more than 1% of requests led to errors
+
+**To run:**
+* Download news.py
+* In command prompt, run "python /PATH/TO/news.py"
+* In IDLE, open "entertainment_center.py" and Run Module
--- Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/news/news.py
+++ Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/news/news.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2.7
-# 
+#
 # Reporting tool on news database
 
 import psycopg2
@@ -13,26 +13,36 @@ c.execute("SELECT title, count(*) AS pageviews "
             "GROUP BY title "
             "ORDER BY pageviews DESC "
             "LIMIT 3;")
-for result in c:
-    print result
+for row in c:
+    print "\"" + row[0] + "\" - " + str(row[1]) + " views"
 print
 
 # 2. Top authors by article views sorted in descending order
 c.execute("SELECT authors.name, count(*) AS totalviews "
             "FROM articles, log, authors "
-            "WHERE log.path LIKE '%' || articles.slug AND authors.id = articles.author "
+            "WHERE log.path LIKE '%' || articles.slug "
+                "AND authors.id = articles.author "
             "GROUP BY name "
             "ORDER BY totalviews DESC;")
-for result in c:
-    print result
+for row in c:
+    print row[0] + " - " + str(row[1]) + " views"
 print
 
 # 3. Days with more than 1% request errors
-c.execute("SELECT * FROM (SELECT errors.day, (cast(errors.visits as decimal) / (errors.visits + successes.visits) * 100) as errorrate FROM (SELECT date(time) as day, count(*) as visits FROM log WHERE status LIKE '4%' GROUP BY day) as errors LEFT JOIN (SELECT date(time) as day, count(*) as visits FROM log WHERE status LIKE '2%' GROUP BY day) as successes ON errors.day = successes.day) as errortable WHERE errorate > 1;")
-
-
-for result in c:
-    print result
+c.execute("SELECT * FROM "
+            "(SELECT errors.day, "
+                "(cast(errors.visits AS decimal) / "
+                    "(errors.visits + successes.visits) * 100) AS errorrate "
+            "FROM (SELECT date(time) AS day, count(*) AS visits "
+                "FROM log WHERE status LIKE '4%' "
+                "GROUP BY day) AS errors "
+            "LEFT JOIN (SELECT date(time) AS day, count(*) AS visits "
+                "FROM log WHERE status LIKE '2%' "
+                "GROUP BY day) AS successes "
+            "ON errors.day = successes.day) AS errortable "
+            "WHERE errorrate > 1;")
+for row in c:
+    print str(row[0]) + " - " + str(round(row[1], 2)) + "% errors"
 print
 
 db.close()
