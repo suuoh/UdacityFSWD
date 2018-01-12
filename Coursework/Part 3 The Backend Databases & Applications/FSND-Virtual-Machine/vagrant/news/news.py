b64dce1d21c340418a750e97a3d09ec6fb7b3ba4
suuoh <me@melvin.io>
2018-01-11 23:17:19 -0500

    Fix pep8 errors

--- Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/news/news.py
+++ Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/news/news.py
@@ -9,38 +9,38 @@ c = db.cursor()
 
 # 1. Top 3 articles by views sorted in descending order
 c.execute("SELECT title, count(*) AS pageviews "
-            "FROM articles, log WHERE log.path LIKE '%' || articles.slug "
-            "GROUP BY title "
-            "ORDER BY pageviews DESC "
-            "LIMIT 3;")
+          "FROM articles, log WHERE log.path LIKE '%' || articles.slug "
+          "GROUP BY title "
+          "ORDER BY pageviews DESC "
+          "LIMIT 3;")
 for row in c:
     print "\"" + row[0] + "\" - " + str(row[1]) + " views"
 print
 
 # 2. Top authors by article views sorted in descending order
 c.execute("SELECT authors.name, count(*) AS totalviews "
-            "FROM articles, log, authors "
-            "WHERE log.path LIKE '%' || articles.slug "
-                "AND authors.id = articles.author "
-            "GROUP BY name "
-            "ORDER BY totalviews DESC;")
+          "FROM articles, log, authors "
+          "WHERE log.path LIKE '%' || articles.slug "
+          "AND authors.id = articles.author "
+          "GROUP BY name "
+          "ORDER BY totalviews DESC;")
 for row in c:
     print row[0] + " - " + str(row[1]) + " views"
 print
 
 # 3. Days with more than 1% request errors
 c.execute("SELECT * FROM "
-            "(SELECT errors.day, "
-                "(cast(errors.visits AS decimal) / "
-                    "(errors.visits + successes.visits) * 100) AS errorrate "
-            "FROM (SELECT date(time) AS day, count(*) AS visits "
-                "FROM log WHERE status LIKE '4%' "
-                "GROUP BY day) AS errors "
-            "LEFT JOIN (SELECT date(time) AS day, count(*) AS visits "
-                "FROM log WHERE status LIKE '2%' "
-                "GROUP BY day) AS successes "
-            "ON errors.day = successes.day) AS errortable "
-            "WHERE errorrate > 1;")
+          "(SELECT errors.day, "
+          "(cast(errors.visits AS decimal) / "
+          "(errors.visits + successes.visits) * 100) AS errorrate "
+          "FROM (SELECT date(time) AS day, count(*) AS visits "
+          "FROM log WHERE status LIKE '4%' "
+          "GROUP BY day) AS errors "
+          "LEFT JOIN (SELECT date(time) AS day, count(*) AS visits "
+          "FROM log WHERE status LIKE '2%' "
+          "GROUP BY day) AS successes "
+          "ON errors.day = successes.day) AS errortable "
+          "WHERE errorrate > 1;")
 for row in c:
     print str(row[0]) + " - " + str(round(row[1], 2)) + "% errors"
 print
