dfc253011b592ab0d0266f552480db62a8d62f07
suuoh <me@melvin.io>
2018-01-12 22:43:50 -0500

    Progress on making a web server

---
+++ Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/restaurant/database_setup.py
@@ -0,0 +1,37 @@
+# Configuration
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# Class
+class Restaurant(Base):
+    # Table
+    __tablename__ = "restaurant"
+
+    # Mapper
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+
+
+class MenuItem(Base):
+    # Table
+    __tablename__ = "menu_item"
+
+    # Mapper
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price  = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey("restaurant.id"))
+    restaurant = relationship(Restaurant)
+
+# Configuration
+engine = create_engine("sqlite:///restaurantmenu.db")
+
+Base.metadata.create_all(engine)
