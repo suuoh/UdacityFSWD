901fbfb3132b39938e76523a5f0249a608cc778c
suuoh <me@melvin.io>
2018-02-12 22:48:52 -0500

    Add function to create new menu items

--- Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/restaurant/project.py
+++ Coursework/Part 3 The Backend Databases & Applications/FSND-Virtual-Machine/vagrant/restaurant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 # Import CRUD
@@ -20,19 +20,25 @@ def restaurantMenu(restaurant_id):
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 # Create route for newMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/new/')
+@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 # Create route for editMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+    return 'page to edit a menu item. Task 2 complete!'
 
 # Create a route for deleteMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+    return 'page to delete a menu item. Task 3 complete!'
 
 if __name__ == '__main__':
     app.debug = True
