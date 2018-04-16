4add3114d788026a9cc1f63bb369fcbd72694ea8
suuoh <me@melvin.io>
2018-04-15 23:38:09 -0400

    Moar cats

--- Coursework/Part 4 The Frontend JavaScript & AJAX/cat-clicker/js/catclicker.js
+++ Coursework/Part 4 The Frontend JavaScript & AJAX/cat-clicker/js/catclicker.js
@@ -1,16 +1,54 @@
-var cat1Name = document.getElementById("cat1-name").textContent = "Joe";
-var cat2Name = document.getElementById("cat2-name").textContent = "Bob";
-var cat1Picture = document.getElementById("cat1-picture");
-var cat2Picture = document.getElementById("cat2-picture");
-var cat1Counter = document.getElementById("cat1-counter");
-var cat2Counter = document.getElementById("cat2-counter");
+var catList = document.getElementById("cat-list");
+var catName = document.getElementById("cat-name");
+var catPicture = document.getElementById("cat-picture");
+var catClicks = document.getElementById("cat-clicks");
+var cats = [{
+    name: "Pumpkin",
+    picture: "https://c1.staticflickr.com/2/1126/625069434_db86b67df8_b.jpg",
+    clicks: 0
+
+},
+{
+    name: "Charlie",
+    picture: "https://c1.staticflickr.com/3/2298/2290467335_89067c7b51_b.jpg",
+    clicks: 0
+},
+{
+    name: "Apples",
+    picture: "https://c1.staticflickr.com/4/3445/5695891871_a390aaa97b_b.jpg",
+    clicks: 0
+},
+{
+    name: "Chocolate",
+    picture: "https://c1.staticflickr.com/4/3236/2892249030_f4d1d6dd48_b.jpg",
+    clicks: 0
+},
+{
+    name: "Snowball",
+    picture: "https://c1.staticflickr.com/4/3607/3406931492_f9420b8659_b.jpg",
+    clicks: 0
+}];
 
 
+for (var i = 0; i < cats.length; i++) {
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    a.href = "#";
+    var aText = document.createTextNode(cats[i].name);
+    a.addEventListener("click", (function(cat) {
+        return function() {
+            catName.textContent = cat.name;
+            catPicture.src = cat.picture;
+            catClicks.textContent = cat.clicks;
+        };
+    })(cats[i]));
+    a.appendChild(aText);
+    li.appendChild(a);
+    catList.appendChild(li);
+}
 
-cat1Picture.addEventListener("click", function() {
-    cat1Counter.textContent++;
-}, false);
-
-cat2Picture.addEventListener("click", function() {
-    cat2Counter.textContent++;
-}, false);
+catPicture.addEventListener("click", function() {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    var cat = cats.find(i => i.name === catName.textContent);
+    catClicks.textContent = ++cat.clicks;
+});
