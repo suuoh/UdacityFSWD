8177e8f459f20df328e80f81f13819567e5e5a0f
suuoh <me@melvin.io>
2018-04-22 23:53:46 -0400

    Add levels function

--- Coursework/Part 4 The Frontend JavaScript & AJAX/L5-16_cat-clicker-knockout/js/app.js
+++ Coursework/Part 4 The Frontend JavaScript & AJAX/L5-16_cat-clicker-knockout/js/app.js
@@ -1,6 +1,16 @@
 var ViewModel = function() {
     this.clicks = ko.observable(0);
     this.name = ko.observable("April");
+    this.level = ko.computed(function() {
+        if (this.clicks() < 10)
+            return "Newborn"
+        else if (this.clicks() < 40)
+            return "Teen"
+        else if (this.clicks() < 100)
+            return "Adult"
+        else if (this.clicks() < 200)
+            return "Senior"
+    }, this);
     this.picture = ko.observable("img/22252709_010df3379e_z.jpg");
     this.attribution = ko.observable("https://flickr.com");
 
