e1effdf3a809c8dbba6e2528294a6624d0e18e85
suuoh <me@melvin.io>
2018-05-29 20:35:10 -0400

    Handle errors more gracefully

--- Part 4 The Frontend JavaScript & AJAX/Neighbourhood Map/app.js
+++ Part 4 The Frontend JavaScript & AJAX/Neighbourhood Map/app.js
@@ -1,173 +1,173 @@
 /*jshint esversion: 6*/
 const PLACES = [
-  {
+{
     id: 0,
     name: "Casa Loma",
     category: "Attraction",
     location: { lat: 43.6780371, lng: -79.4094439 },
     place_id: "ChIJs6Elz500K4gRT1jWAsHIfGE",
     address: "1 Austin Terrace, Toronto, ON M5R 1X8"
-  },
-  {
+},
+{
     id: 1,
     name: "Chase Fish and Oyster",
     category: "Restaurant",
     location: { lat: 43.651082, lng: -79.379378 },
     place_id: "ChIJ13xO78EzK4gRdwEiU8N4oCk",
     address: "10 Temperance St, first floor, Toronto, ON M5H 1Y4"
-  },
-  {
+},
+{
     id: 2,
     name: "CN Tower",
     category: "Attraction",
     location: { lat: 43.6425662, lng: -79.3870568 },
     place_id: "ChIJmzrzi9Y0K4gRgXUc3sTY7RU",
     address: "301 Front St W, Toronto, ON M5V 2T6"
-  },
-  {
+},
+{
     id: 3,
     name: "Miku",
     category: "Restaurant",
     location: { lat: 43.6412346, lng: -79.3773697 },
     place_id: "ChIJbRVhVirL1IkRKu9XddGTiKU",
     address: "10 Bay St # 105, Toronto, ON M5J 2R8"
-  },
-  {
+},
+{
     id: 4,
     name: "Ripley's Aquarium of Canada",
     category: "Attraction",
     location: { lat: 43.64240299999999, lng: -79.385971 },
     place_id: "ChIJWwS21dU0K4gRPSGMKRkar40",
     address: "288 Bremner Blvd, Toronto, ON M5V 3L9"
-  },
-  {
+},
+{
     id: 5,
     name: "Royal Ontario Museum",
     category: "Attraction",
     location: { lat: 43.6677097, lng: -79.3947771 },
     place_id: "ChIJE-Xa87o0K4gRkvXFHuE0hMk",
     address: "100 Queens Park, Toronto, ON M5S 2C6"
-  },
-  {
+},
+{
     id: 6,
     name: "Snakes & Lattes",
     category: "Entertainment",
     location: { lat: 43.6649006, lng: -79.4131884 },
     place_id: "ChIJc-8hbJI0K4gRDhU_qf-1yII",
     address: "600 Bloor St W, Toronto, ON M6G 1K4"
-  },
-  {
+},
+{
     id: 7,
     name: "Terroni",
     category: "Restaurant",
     location: { lat: 43.650916, lng: -79.375685 },
     place_id: "ChIJT1kRzTPL1IkRqYD-w3lcprg",
     address: "57 Adelaide St E, Toronto, ON M5C 1K6"
-  },
-  {
+},
+{
     id: 8,
     name: "TIFF Bell Lightbox",
     category: "Entertainment",
     location: { lat: 43.6465295, lng: -79.3904082 },
     place_id: "ChIJewSvlNA0K4gRlkERDVrMZ08",
     address: "350 King St W, Toronto, ON M5V 3X5"
-  },
-  {
+},
+{
     id: 9,
     name: "Toronto City Hall",
     category: "Attraction",
     location: { lat: 43.6534399, lng: -79.38409009999999 },
     place_id: "ChIJ81rnZsw0K4gR4CIzkYYawjE",
     address: "100 Queen St W, Toronto, ON M5H 2N2"
-  },
-  {
+},
+{
     id: 10,
     name: "University of Toronto",
     category: "School",
     location: { lat: 43.6628917, lng: -79.39565640000001 },
     place_id: "ChIJm_0x87g0K4gR93ZadrabHY0",
     address: "27 King's College Cir, Toronto, ON M5S 3H7"
-  }
+}
 ];
 const MAP_STYLES = [
-  {
+{
     featureType: "administrative",
     elementType: "labels.text.fill",
     stylers: [
-      {
+    {
         color: "#444444"
-      }
+    }
     ]
-  },
-  {
+},
+{
     featureType: "landscape",
     elementType: "all",
     stylers: [
-      {
+    {
         color: "#f2f2f2"
-      }
+    }
     ]
-  },
-  {
+},
+{
     featureType: "poi",
     elementType: "all",
     stylers: [
-      {
+    {
         visibility: "off"
-      }
+    }
     ]
-  },
-  {
+},
+{
     featureType: "road",
     elementType: "all",
     stylers: [
-      {
+    {
         saturation: -100
-      },
-      {
+    },
+    {
         lightness: 45
-      }
+    }
     ]
-  },
-  {
+},
+{
     featureType: "road.highway",
     elementType: "all",
     stylers: [
-      {
+    {
         visibility: "simplified"
-      }
+    }
     ]
-  },
-  {
+},
+{
     featureType: "road.arterial",
     elementType: "labels.icon",
     stylers: [
-      {
+    {
         visibility: "off"
-      }
+    }
     ]
-  },
-  {
+},
+{
     featureType: "transit",
     elementType: "all",
     stylers: [
-      {
+    {
         visibility: "off"
-      }
+    }
     ]
-  },
-  {
+},
+{
     featureType: "water",
     elementType: "all",
     stylers: [
-      {
+    {
         color: "#46bcec"
-      },
-      {
+    },
+    {
         visibility: "on"
-      }
+    }
     ]
-  }
+}
 ];
 
 var map;
@@ -178,19 +178,19 @@ var infoWindow;
 function initMap() {
     // Initialize map
     map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: 43.659638669778936, lng: -79.39443669999997},
+        center: { lat: 43.659638669778936, lng: -79.39443669999997 },
         zoom: 14,
-        styles: mapStyles,
+        styles: MAP_STYLES,
         mapTypeControl: false
     });
 
-    for (var i = 0; i < places.length; i++) {
+    PLACES.forEach(function(place) {
         // Create marker in map
         var marker = new google.maps.Marker({
-            position: places[i].location,
-            title: places[i].name,
+            position: place.location,
+            title: place.name,
             animation: google.maps.Animation.DROP,
-            id: i,
+            id: place.id,
             map: map
         });
 
@@ -202,7 +202,7 @@ function initMap() {
         marker.addListener("click", function() {
             populateInfoWindow(this, infoWindow);
         });
-    }
+    });
 }
 
 var ViewModel = function() {
@@ -217,7 +217,7 @@ var ViewModel = function() {
         ]);
 
     self.results = ko.observableArray([]);
-    places.forEach(function(place) {
+    PLACES.forEach(function(place) {
         self.results.push(place);
     });
 
@@ -237,9 +237,9 @@ var ViewModel = function() {
         self.results.removeAll();
         bounds = new google.maps.LatLngBounds();
 
-        places.forEach(function(place) {
+        PLACES.forEach(function(place) {
             marker = markers[place.id];
-            marker.setMap(map);
+            marker.setVisible(true);
             bounds.extend(marker.position);
             self.results.push(place);
         });
@@ -253,15 +253,15 @@ var ViewModel = function() {
         bounds = new google.maps.LatLngBounds();
 
         // Find places with matching category
-        for (var i = 0; i < places.length; i++) {
-            if (places[i].category === category) {
-                markers[i].setMap(map);
-                bounds.extend(markers[i].position);
-                self.results.push(places[i]);
+        PLACES.forEach(function(place) {
+            if (place.category === category) {
+                markers[place.id].setVisible(true);
+                bounds.extend(markers[place.id].position);
+                self.results.push(place);
             } else {
-                markers[i].setMap(null);
+                markers[place.id].setVisible(false);
             }
-        }
+        });
 
         updateMapBounds();
     };
@@ -269,17 +269,14 @@ var ViewModel = function() {
     // Toggle sidebar (on small screens only)
     self.sidebarVisible = ko.observable(true);
     self.toggleSidebar = function() {
-        if (self.sidebarVisible())
-            self.sidebarVisible(false);
-        else
-            self.sidebarVisible(true);
+        self.sidebarVisible(!self.sidebarVisible());
     };
 };
 
 // Create and open infowindow on a given marker
 function populateInfoWindow(marker, infowindow) {
     if (infowindow.marker != marker) {
-        var place = places[marker.id];
+        var place = PLACES[marker.id];
         infowindow.marker = marker;
         var infoContent = "";
 
@@ -287,16 +284,17 @@ function populateInfoWindow(marker, infowindow) {
         infoContent += "<h4>" + place.name + "</h4>"; // Name of place
         infoContent += "<p><em>" + place.category + "</em></p>"; // Category
         infoContent += "<p>" + place.address + "</p>"; // Street address
-        infoContent +=
-        "<p><a href='https://www.google.com/maps/dir/?api=1&destination=";
+        infoContent += "<p><a href='https://www.google.com/maps/dir/?api=1&destination=";
         infoContent += place.name + "&destination_place_id=" + place.place_id;
         infoContent += "' target='_blank'>Get Directions</a>"; // Google Maps directions
         // If it exists, include Wikipedia link
-        if (place.wikipedia)
-            infoContent +=
-        "<p><a href='" +
-        place.wikipedia +
-        "' target='_blank'>Wikipedia</a></p>";
+        if (place.wikipedia) {
+            // Check if Wikipedia library call resulted in error
+            if (place.wikipedia !== "Error")
+                infoContent += "<p><a href='" +  place.wikipedia + "' target='_blank'>Wikipedia</a></p>";
+            else
+                infoContent += "<p><strong>Error:</strong> Wikipedia API could not be loaded.</p>";
+        }
 
         infowindow.setContent(infoContent);
         infowindow.open(map, marker);
@@ -342,31 +340,27 @@ function getWikiLinks() {
     wikiURL += "action=opensearch&format=json";
     wikiURL += "&search=";
 
-    places.forEach(function(place) {
+    PLACES.forEach(function(place) {
         if (place.category !== "Restaurant") {
             $.ajax({
                 url: wikiURL + place.name,
-                dataType: "jsonp",
-                success: function(data) {
-                    place.wikipedia =
-                    "https://en.wikipedia.org/wiki/" + data[1][0];
-                },
-                timeout: 5000, // JSONP requests do not fire jQuery's error event, so a timeout is set instead
-                error: function(data) {
-                    libraryError("Wikipedia", false); // App and interface still function fine if Wikipedia fails to load
-                }
+                dataType: "jsonp"
+            })
+            .done(function(data) {
+                place.wikipedia =  "https://en.wikipedia.org/wiki/" + data[1][0];
+            })
+            .fail(function() {
+                place.wikipedia = "Error";
+                console.log("Oops! There was an issue loading the Wikipedia library. Please try again by refreshing the page.");
             });
         }
     });
 }
 
-// Show alert and log errors when API libraries cannot be loaded
-function libraryError(library, popup) {
-    var message = "Oops! There was an issue loading the " + library + " library. Please try again by refreshing the page.";
-    if (popup)
-        alert(message);
-    console.log(message);
-    return false;
+// Log and show message if Google Maps library fails
+function gmapsError() {
+    console.log("Oops! There was an issue loading the Google Maps library. Please try again by refreshing the page.");
+    document.getElementById("map").innerHTML = "<h4>Error: Google Maps API could not be loaded.</h4>";
 }
 
 // Apply Knockout bindings when DOM is loaded
